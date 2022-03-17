391c28d46fbaaadffbc6bfdd2a544f62cfced48c
Alexander Asis <alexander.asis@cvsu.edu.ph>
2022-03-17 19:46:37 +0800

    Added a few modifications

--- DamageCalculator.py
+++ DamageCalculator.py
@@ -8,17 +8,26 @@ PkmnName = input("Name of the Pokemon: ")
 PkmnLevel = int(input("What is your Pokemon's Level: "))
 UserPokemonType1 = input("Input Pokemon's First Type: ")
 UserPokemonType2 = input("Input Pokemon's Second Type (type 'n' if none): ")
-    
+ASpecial = int(input("Special Attack Stat: "))
+EnemyPkmnName = input("Enemy Pokemon Name: ")
+EnemyPkmnType1 = input("Enemy Pokemon Type 1: ")
+EnemyPkmnType2 = input("Enemy Pokemon Type 2: ")
+DDef = int(input("Special Defense of Enemy Pokemon: "))
+
+PkmnTypeList = ["Fire","Ice","Water","Ground","Rock","Psychic","Ghost","Dark","Fairy","Steel","Grass","Fighting","Flying","Dragon","Bug","Poison","Electric","Normal"]
+EnemyPkmnTypeList = [EnemyPkmnType1,EnemyPkmnType2]    
 UserPokemonTypeList = [UserPokemonType1,UserPokemonType2]
 
 MoveName = input("Name of move: ")
 MoveType = input("Type of move: ")
+Power = int(input("Power of attack: "))
+
+BurnStat = input("Is your pokemon under BURN status? Y/N: ")
 Burn = 0
-BurnStat = (input("Is your pokemon under BURN status? Y/N: "))
-if BurnStat == 'Y' or 'y':
-    Burn == 0.5
-elif BurnStat == 'N' or 'n':
-    Burn == 1
+if BurnStat == 'y' or BurnStat == 'Y':
+    Burn = 0.5
+elif BurnStat == 'n' or BurnStat == 'N':
+    Burn = 1
 NoOfTargets = int(input("Number of enemies: "))
 if NoOfTargets == 1:
     Targets = 1
@@ -35,6 +44,7 @@ elif WeatherType == "Rain" and MoveType == "Water":
 elif WeatherType == "Rain" and MoveType == "Fire":
         Weather = 0.5
 elif any(WeatherType in s for s in WeatherList):
+    
         Weather = 1
 else:
     print("Weather Type Unknown! Result will be 0")
@@ -47,13 +57,33 @@ if any(MoveType in s for s in UserPokemonTypeList):
 else:
     STAB = 1
     
-Type = 2
+Type = 0
+if MoveType == "Fire" and (EnemyPkmnType1 == "Grass" or EnemyPkmnType1 == "Ice" or EnemyPkmnType1 == "Bug" or EnemyPkmnType1 == "Steel"):
+    Type = 2
+    if Type == 2 and (EnemyPkmnType2 == "Grass" or EnemyPkmnType2 == "Ice" or EnemyPkmnType2 == "Bug" or EnemyPkmnType2 == "Steel"):
+        Type += 2
+    elif Type == 2 and (EnemyPkmnType2 == "Water" or EnemyPkmnType2 == "Ground" or EnemyPkmnType2 == "Rock"):
+        Type = 1
+    elif Type == 1and (EnemyPkmnType2 == "Nullifier"):
+        Type = 0
+elif MoveType == "Fire" and (EnemyPkmnType1 == "Water" or EnemyPkmnType1 == "Ground" or EnemyPkmnType1 == "Rock"):
+    Type = 0.5
+    if Type == 0.5 and (EnemyPkmnType2 == "Water" or EnemyPkmnType2 == "Ground" or EnemyPkmnType2 == "Rock"):
+        Type = 0.25
+elif MoveType == "Fire" and EnemyPkmnType1 == "Nullifier":
+    Type = 0
+elif MoveType not in PkmnTypeList:
+    print("Invalid Type")
+
+Other = 1
 modifier = Targets * Weather * Badge * Critical * rendom * STAB * Type * Burn * Other
-Damage = ((((((2*PkmnLevel)/5)+2)*(110)*(205/180))/50)+2) * modifier
+Damage = ((((((2*PkmnLevel)/5)+2)*(Power)*(ASpecial/DDef))/50)+2) * modifier
 print(round(Damage))
 print(Weather)
 print(Targets)
 print(UserPokemonTypeList)
 print(STAB)
+print(Type)
+print(Burn)
 
 
